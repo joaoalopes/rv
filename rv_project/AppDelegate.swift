a51f96eb244fe9804f4e1b7904cc2a222cff43c2
joaoalopes <joao.amaral.lopes@tecnico.ulisboa.pt>
2019-10-23 16:45:24 +0100

    Teste

--- rv_project/AppDelegate.swift
+++ rv_project/AppDelegate.swift
@@ -12,8 +12,7 @@ import SwiftUI
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
+    //var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
